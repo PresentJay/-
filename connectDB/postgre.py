883f073ba3242260e864274581f059c85c390d44
PresentJay <presentj94@gmail.com>
2021-01-13 13:00:03 +0900

    [feature] aws rds postresql과 통신 성공

--- connectDB/postgre.py
+++ connectDB/postgre.py
@@ -8,11 +8,24 @@ def connect(h, d, u, pw, p):
     try:
         conn = psycopg2.connect(host=h, user=u, password=pw, dbname=d, port=p)
         cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        print("PostgreSQL server information")
+        print(conn.get_dsn_parameters(), "\n")
+        # Executing a SQL query
+        cur.execute("SELECT version();")
+        # Fetch result
+        record = cur.fetchone()
+        print("You are connected to - ", record, "\n")
         
-        print(query)
+        #print(query)
+        #cur.execute(query)
+        #conn.commit()
         
-        cur.execute(query)
-        conn.commit()
+    except (Exception, psycopg2.Error) as error:
+        print('Fail to connect DB >>', error)
         
-    except Exception:
-        print('Fail to connect DB')
+    finally:
+        if (conn):
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
